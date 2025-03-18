94933669ee008b407a66e1df85b85108822ff741
abulanda <bulandaa3@gmail.com>
2025-03-18 21:46:06 +0100

    fix(config): remove warnings

--- backend/src/main/java/com/tripmakin/config/WebConfig.java
+++ backend/src/main/java/com/tripmakin/config/WebConfig.java
@@ -5,13 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.lang.NonNull;
+
+
 @Configuration
 public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
